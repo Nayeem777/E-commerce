e373b534c7afa673148b8f1229237d0645040f9e
tusharwdev <tusharwdev@gmail.com>
2021-08-21 08:43:45 +0600

    ecommerce frontend Template Done

---
+++ include/head.php
@@ -0,0 +1,34 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- ==== Document Title ==== -->
+<title>shared on themelock.com</title>
+
+<!-- ==== Document Meta ==== -->
+<meta name="author" content="">
+<meta name="description" content="">
+<meta name="keywords" content="">
+
+<!-- ==== Favicon ==== -->
+<link rel="icon" href="favicon.png" type="image/png">
+
+<!-- ==== Google Font ==== -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700%7CMontserrat:400,500">
+
+<!-- Stylesheets -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/fontawesome-all.min.css">
+<link rel="stylesheet" href="css/jquery-ui.min.css">
+<link rel="stylesheet" href="css/perfect-scrollbar.min.css">
+<link rel="stylesheet" href="css/morris.min.css">
+<link rel="stylesheet" href="css/select2.min.css">
+<link rel="stylesheet" href="css/jquery-jvectormap.min.css">
+<link rel="stylesheet" href="css/horizontal-timeline.min.css">
+<link rel="stylesheet" href="css/weather-icons.min.css">
+<link rel="stylesheet" href="css/dropzone.min.css">
+<link rel="stylesheet" href="css/ion.rangeSlider.min.css">
+<link rel="stylesheet" href="css/ion.rangeSlider.skinFlat.min.css">
+<link rel="stylesheet" href="css/datatables.min.css">
+<link rel="stylesheet" href="css/fullcalendar.min.css">
+<link rel="stylesheet" href="css/style.css">
---
+++ include/script.php
@@ -0,0 +1,20 @@
+<!-- Scripts -->
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/perfect-scrollbar.min.js"></script>
+<script src="js/jquery.sparkline.min.js"></script>
+<script src=" js/raphael.min.js"></script>
+<script src=" js/morris.min.js"></script>
+<script src=" js/select2.min.js"></script>
+<script src=" js/jquery-jvectormap.min.js"></script>
+<script src=" js/jquery-jvectormap-world-mill.min.js"></script>
+<script src=" js/horizontal-timeline.min.js"></script>
+<script src=" js/jquery.validate.min.js"></script>
+<script src=" js/jquery.steps.min.js"></script>
+<script src=" js/dropzone.min.js"></script>
+<script src=" js/ion.rangeSlider.min.js"></script>
+<script src=" js/datatables.min.js"></script>
+<script src=" js/main.js"></script>
+
+<!-- Page Level Scripts -->
